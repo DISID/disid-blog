419bdd4f73440c38b96197ea9f5b3966f6aa1e21
Pilar Argudo <pargudo@disid.com>
2015-12-04 14:05:15 +0100

    Built from 416a98c

--- assets/js/jquery.cookiecuttr.js
+++ assets/js/jquery.cookiecuttr.js
@@ -33,11 +33,11 @@
             cookieAcceptButton: true, // this will disable non essential cookies
             cookieResetButton: false,
             cookieOverlayEnabled: false, // don't want a discreet toolbar? Fine, set this to true
-            cookiePolicyLink: '/en/cookies-policy/', // if applicable, enter the link to your privacy policy here...
+            cookiePolicyLink: 'http://www.disid.com/en/politica-de-cookies', // if applicable, enter the link to your privacy policy here...
             cookieMessage: 'We use cookies on this website, you can <a href="{{cookiePolicyLink}}" title="read about our cookies">read about them here</a>. To use the website as intended please...',
             cookieAnalyticsMessage: "This site uses own and third-party cookies just to track visits to our website, we store no personal details. If you continue to browse, consider accepting its use.",
             cookieErrorMessage: "We\'re sorry, this feature places cookies in your browser and has been disabled. <br>To continue using this functionality, please",
-            cookieWhatAreTheyLink: "www.disid.com/en/politica-de-cookies",
+            cookieWhatAreTheyLink: "http://www.disid.com/en/politica-de-cookies",
             cookieDisable: '',
             cookieExpires: 365,
             cookieAcceptButtonText: "Accept cookies",
